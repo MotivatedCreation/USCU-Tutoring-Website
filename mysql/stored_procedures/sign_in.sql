a72abdcfdfc700003af9d2417b08e8c23072d055
Jonathan Sullivan <jonsullivan@live.com>
2016-02-10 17:51:22 -0500

    Initial Commit

--- mysql/stored_procedures/sign_in.sql
+++ mysql/stored_procedures/sign_in.sql
@@ -2,9 +2,11 @@ DELIMITER //
 
 CREATE PROCEDURE sign_in
 (IN userPassword VARCHAR(128),
-	userEmail VARCHAR(128))
+		userEmail VARCHAR(128))
 BEGIN
 	SELECT email
 	FROM User
 	WHERE email = userEmail;
-END//
+END
+
+//
